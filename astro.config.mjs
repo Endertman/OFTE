2e1434b30f5e6ce73776003a0ba07520dd6a7667
Endert Guerrero <e.guerrerocamacho@uandresbello.edu>
2025-06-03 22:51:45 -0400

    Actualiza la configuración de Firebase para establecer la carpeta 'dist' como fuente de hosting y agrega reglas de reescritura para redirigir todas las solicitudes a index.html. Cambia la salida de Astro a 'static' y elimina el adaptador de Node.js, simplificando la estructura del proyecto.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   viewTransitions: true,
   integrations: [tailwind(), compress()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static'
 });
